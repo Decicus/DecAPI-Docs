32f8a6f4eb8ebf5c290eefafcf79a4f17db60d2f
Alex Thomassen <alex@thomassen.xyz>
2020-07-28 21:21:53 +0200

    [Changelog] Changes in v2.0.1

--- src/components/Changelog.vue
+++ src/components/Changelog.vue
@@ -10,6 +10,33 @@
             However, you can browse the <a href="https://github.com/Decicus/DecAPI/commits/d4a849c7d0131ac74d64938e7850f1badb77335a">commit log prior to V2.0.0 on GitHub</a>, which should give you a decent overview.
         </p>
 
+        <div class="card" id="v2.0.1">
+            <h4 class="card-header bg-primary text-white">
+                Version 2.0.1 - Minor changes
+
+                <span class="badge badge-secondary float-right" title="July 28th, 2020">2020-07-28</span>
+            </h4>
+
+            <div class="card-body">
+                <h5><i class="fab fa-1x fa-fw fa-twitch"></i> Twitch</h5>
+                <ul>
+                    <li>
+                        <strong>Backend changes:</strong> The new Twitch API (Helix) is now used to retrieve a channel's game or stream title.
+                    </li>
+                </ul>
+
+                <h5><i class="fas fa-1x fa-fw fa-code"></i> Laravel Framework - Security Update</h5>
+                <ul>
+                    <li>
+                        Laravel released a <a href="https://blog.laravel.com/laravel-cookie-security-releases">security update</a> and DecAPI's dependencies have been updated to use a patched version of the framework.
+                        <ul>
+                            <li>The live version should not be affected by this regardless, as it does not save any session data in cookies.</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
         <div class="card" id="v2.0.0">
             <h4 class="card-header bg-primary text-white">
                 Version 2.0.0 - Official Release!
